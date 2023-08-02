05b12ac46cb00fae870706eb1e47d3da7e548c7d
Edson Toledo <edsontoledojunior@hotmail.com>
2023-08-01 21:06:24 -0300

    EdsonToledo

---
+++ tblalunos.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 02/08/2023 às 00:23
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bdedsontoledo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tblalunos`
+--
+
+CREATE TABLE `tblalunos` (
+  `idalunos` int(11) NOT NULL,
+  `aluno` varchar(50) NOT NULL,
+  `datanasc` date NOT NULL,
+  `mensalidade` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
